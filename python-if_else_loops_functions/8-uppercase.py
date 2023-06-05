f906a2ee67718970be19801aaa1e5209e213c54d
Axel Malka <axelmalka@outlook.fr>
2023-06-05 12:02:22 +0000

    function that switch lowercase in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        char = ord(letter)
+        if char >= 97 and char <= 122:
+            char = char - 32
+        print("{}".format(chr(char)), end="")
+    print()
