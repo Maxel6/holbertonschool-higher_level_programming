8844032dc40550ff82143efee73987796426d751
Axel Malka <axelmalka@outlook.fr>
2023-06-05 14:51:20 +0000

    args of python script

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    argc = len(argv)
+    sentence = "arguments"
+    if argc == 1:
+        sentence = "argument:"
+    if argc > 1:
+        sentence = "arguments:"
+    print("{} {}".format(argc, sentence))
+    for i in range (argc):
+        print("{}: {}".format(i + 1, argv[i]))
