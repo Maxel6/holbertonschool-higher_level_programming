c23e1d29257f692f030e74a32adcc27165530b62
Axel Malka <axelmalka@outlook.fr>
2023-06-06 15:20:03 +0000

    fix issue with one integer in the list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
-        b = 0
+    if my_list != []:
+        b = my_list[0]
         for a in my_list:
             if b < a:
                 b = a
