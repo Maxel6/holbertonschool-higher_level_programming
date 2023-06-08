fbe20d871709430aa4a1a5df808fee8fb3ed0961
Axel Malka <axelmalka@outlook.fr>
2023-06-08 08:59:16 +0000

    adding null list case

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    if my_list:
-        for num in my_list:
-            if num == search:
-                new_list.append(replace)
-            else:
-                new_list.append(num)
-        return new_list
-    return None
+    if my_list == []:
+        return None
+
+    for num in my_list:
+        if num == search:
+            new_list.append(replace)
+        else:
+            new_list.append(num)
+    return new_list
