41cc339d15cf6a9dbfe94d319cd2b964cf0a64f4
Axel Malka <axelmalka@outlook.fr>
2023-06-08 09:00:16 +0000

    adding null list case

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    if my_list == []:
+    if my_list == 0 or my_list is None:
         return None
 
     for num in my_list:
