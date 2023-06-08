db1d4e7cf07242827846afd19be20c53abb4f3f4
Axel Malka <axelmalka@outlook.fr>
2023-06-08 08:55:32 +0000

    fix issues with new list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = []
     if my_list:
-        for i in range(0, len(my_list)):
-            if my_list[i] == search:
-                my_list[i] = replace
-
-        return my_list
+        for num in my_list:
+            if num == search:
+                new_list.append(replace)
+            else:
+                new_list.append(num)
+        return new_list
