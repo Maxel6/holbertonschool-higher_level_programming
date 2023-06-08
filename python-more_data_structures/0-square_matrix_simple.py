1a438343b7a2b1d0d27a0c35cfce69066d849b87
Axel Malka <axelmalka@outlook.fr>
2023-06-08 13:01:49 +0000

    square matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix == []:
+        return matrix
+    squared_matrix = [[element**2 for element in row] for row in matrix]
+    return squared_matrix
