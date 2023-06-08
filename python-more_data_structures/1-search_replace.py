85f75817fb9a2c1150dec2792c557f5221eb2e8b
Axel Malka <axelmalka@outlook.fr>
2023-06-08 08:45:40 +0000

    pycodestyle corrected

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,5 +4,5 @@ def search_replace(my_list, search, replace):
         for i in range(0, len(my_list)):
             if my_list[i] == search:
                 my_list[i] = replace
- 
+
         return my_list
