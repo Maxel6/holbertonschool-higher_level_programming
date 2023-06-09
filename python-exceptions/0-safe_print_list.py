4017357bcebde485690739e2d3a958d03c995467
Axel Malka <axelmalka@outlook.fr>
2023-06-09 10:59:51 +0000

    adding .format

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,10 +4,19 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             if i < x:
-                print(my_list[i], end="")
+                print("{}".format(my_list[i]), end="")
                 count += 1
         print()
         return count
     except IndexError:
         print()
-        return 0
+        return count
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
