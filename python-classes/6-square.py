165428adb58dd7bafe1cdab95654f24a24e79ed8
Axel Malka <axelmalka@outlook.fr>
2023-06-13 09:06:18 +0000

    adding space in print

---
+++ python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """init square with error"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (value[0] or value[1]) < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for space in range(0, self.__position[1]):
+                print()
+            for y in range(0, self.__size):
+                for xspace in range(0, self.__position[0]):
+                    print(" ", end="")
+                for x in range(0, self.__size):
+                    print("#", end="")
+                print()
