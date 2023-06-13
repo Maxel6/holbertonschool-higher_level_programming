02465ec67ee591da5de5b4c8be1300dc36c3187e
Axel Malka <axelmalka@outlook.fr>
2023-06-13 09:29:32 +0000

    change raise comdition

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,9 +31,7 @@ class Square:
     def position(self, value):
         if type(value) != tuple or len(value) < 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if (value[0] or value[1]) < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(num, int) for num in value):
+        if not all(isinstance(num, int) and num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
