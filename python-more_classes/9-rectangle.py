5b2f84267c13e3cfee6174a4c6d76bd355c2b400
Axel Malka <axelmalka@outlook.fr>
2023-06-15 11:48:46 +0000

    remove test

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -82,7 +82,3 @@ class Rectangle:
     def square(cls, size=0):
         if size is not None:
             return cls(size, size)
-
-my_square = Rectangle.square(5)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
