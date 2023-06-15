b6eb79d1795dc6fa24bc71fb2c829371f28ea156
Axel Malka <axelmalka@outlook.fr>
2023-06-15 11:25:54 +0000

    corrected pycodestyle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -66,13 +66,13 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
 
     def bigger_or_equal(rect_1, rect_2):
- 
+
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        
+
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        
+
         if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
             return rect_1
         else:
