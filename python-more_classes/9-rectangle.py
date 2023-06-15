1f214fb7277094972f06b4b9749c8782d7b8c661
Axel Malka <axelmalka@outlook.fr>
2023-06-15 11:45:28 +0000

    unsing cls

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -77,7 +77,6 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
-
-    @classmethod
+    
     def square(cls, size=0):
-        return Rectangle(size, size)
+        return Rectangle(cls, cls)
