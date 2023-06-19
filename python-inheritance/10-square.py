fe3ff41d41d7a41da586b30030f8d6634d70e6db
Axel Malka <axelmalka@outlook.fr>
2023-06-19 15:43:43 +0000

    corrected pycodestyle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -34,6 +34,7 @@ class Square(Rectangle):
     def __init__(self, size):
         BaseGeometry.integer_validator(self, "size", size)
         self.__size = size
+
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__size, self.__size)
 
