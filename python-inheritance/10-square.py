09324dbdf8d14b3cade1e2c4a37c3ca0035dfdbc
Axel Malka <axelmalka@outlook.fr>
2023-06-19 15:43:31 +0000

    adding __str__ method

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -34,6 +34,8 @@ class Square(Rectangle):
     def __init__(self, size):
         BaseGeometry.integer_validator(self, "size", size)
         self.__size = size
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
 
     def area(self):
         return self.__size ** 2
