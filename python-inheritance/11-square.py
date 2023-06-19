5ae9d1b5fcbd9706be60dca33e7a4b2b5e2ad252
Axel Malka <axelmalka@outlook.fr>
2023-06-19 15:45:46 +0000

    replace square in str

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+class BaseGeometry():
+    """BaseGeometry"""
+    def area(self, width, height):
+        return width * height
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle"""
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+
+
+class Square(Rectangle):
+    """Square"""
+    def __init__(self, size):
+        BaseGeometry.integer_validator(self, "size", size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        return self.__size ** 2
