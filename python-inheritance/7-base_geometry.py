dfc278de7ec12944a8a3d5917184bbe4eb0395b4
Axel Malka <axelmalka@outlook.fr>
2023-06-19 09:40:19 +0000

    change type check

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -2,13 +2,13 @@
 """BaseGeometry"""
 
 
-class BaseGeometry:
-    """BaseGeometry class"""
+class BaseGeometry():
+    """BaseGeometry"""
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
