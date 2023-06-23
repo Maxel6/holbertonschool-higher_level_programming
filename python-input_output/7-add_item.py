4bc544014286a786ffd38c31aa6e4276c6c92bfd
Axel Malka <axelmalka@outlook.fr>
2023-06-23 08:35:00 +0000

    fix issus with empty list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 args = sys.argv[1:]
-my_list = args
+my_list = []
 filename = "add_item.json"
 if os.path.exists(filename):
     my_list = load_from_json_file(filename)
