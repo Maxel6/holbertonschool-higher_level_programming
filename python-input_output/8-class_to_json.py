fa4ab1a69c7025323bae058b23b57de8d11c5f42
Axel Malka <axelmalka@outlook.fr>
2023-06-23 08:43:41 +0000

    return __dict__

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import json
 """Class to JSON"""
 
 
 def class_to_json(obj):
-    """eturns the dictionary description with simple data structure for \
+    """Returns the dictionary description with simple data structure for \
     JSON serialization of an object"""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
