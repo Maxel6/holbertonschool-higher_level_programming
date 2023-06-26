2ea6f6c62e1f9c7d5f55be9ecf08a674f46c5911
Axel Malka <axelmalka@outlook.fr>
2023-06-26 08:12:09 +0000

    init commit + base model structure

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Base class"""
+
+
+class Base:
+    """Base Class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_object += 1
+            self.id = self.__nb_object
