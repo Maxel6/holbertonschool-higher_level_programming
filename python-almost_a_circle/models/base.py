8f2478c13c45d3720e4d7d54ca97be73a6867da7
Axel Malka <axelmalka@outlook.fr>
2023-06-28 11:49:19 +0000

    rretur the literaly str []

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,8 +22,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """return the Json  str representation of list_dictionary"""
-
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
-
+        if not list_dictionaries or len(list_dictionaries) == 0:
+            return "[]"
+        
         return json.dumps(list_dictionaries)
