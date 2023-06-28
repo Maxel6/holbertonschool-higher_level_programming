481799b5baba798456de7022abc8b7b8c749e849
Axel Malka <axelmalka@outlook.fr>
2023-06-28 11:45:09 +0000

    adding staticmethod decorator

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Import Json lib"""
 import json
 """Base class"""
 
@@ -8,12 +9,16 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Constructor method for the Base class.
+        Args:
+            id (int): Positive integer to set class instance's id to.
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+    @staticmethod
     def to_json_string(list_dictionaries):
         """return the Json  str representation of list_dictionary"""
         return json.dumps(list_dictionaries)
