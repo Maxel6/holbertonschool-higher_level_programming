24bbf1941ac14787dede2ff447f3a158e7139586
Axel Malka <axelmalka@outlook.fr>
2023-08-02 15:48:36 +0000

    filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Takes in an argument and displays all values in the states table"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    state_name = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' \
+                ORDER BY states.id".format(state_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+#!/usr/bin/python3
+"""Takes in an argument and displays all values in the states table"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    state_name = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' \
+                ORDER BY states.id".format(state_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
