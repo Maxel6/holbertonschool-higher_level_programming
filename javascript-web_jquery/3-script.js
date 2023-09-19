70d2bc7ea9b68941da1a051505fdcd6e3cbddb5e
Axel Malka <axelmalka@outlook.fr>
2023-09-19 07:19:53 +0000

    discover Jquery

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Add a click event listener to the DIV#red_header element
+    $('#red_header').click(function() {
+      // Add the 'red' class to the <header> element
+      $('header').addClass('red');
+    });
+  });
